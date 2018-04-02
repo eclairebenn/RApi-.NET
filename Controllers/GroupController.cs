af568863128eb567be29aaaa7b7357170f9b538f
Noelle <noelle.teh@gmail.com>
2018-04-02 14:43:47 -0700

    initial commit

---
+++ Controllers/GroupController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RapperAPI.Controllers {
+    public class GroupController : Controller {
+        List<Group> allGroups {get; set;}
+        public GroupController() {
+            allGroups = JsonToFile<Group>.ReadJson();
+        }
+
+    }
+}
